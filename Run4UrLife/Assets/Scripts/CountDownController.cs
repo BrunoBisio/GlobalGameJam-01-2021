8ea04b2d12fb70f12d92513ff17ce7c0ba92dd0a
alexis <alexis.joel.sessarego@gmail.com>
2021-01-30 17:34:17 -0300

    added some sprites and timer

---
+++ Run4UrLife/Assets/Scripts/CountDownController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownController : MonoBehaviour
+{
+    public TimerService timer;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.text.text = buildCountDownText();
+    }
+
+    string buildCountDownText()
+    {
+        return timer.getMinutes() + ":" + timer.getSeconds();
+    }
+}
---
+++ Run4UrLife/Assets/Scripts/TimerService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerService : MonoBehaviour
+{
+    public float timeLeft = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft<= 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    public int getSeconds()
+    {
+        return Mathf.FloorToInt(timeLeft % 60);
+    }
+
+    public int getMinutes()
+    {
+        return Mathf.FloorToInt(timeLeft / 60);
+    }
+}
